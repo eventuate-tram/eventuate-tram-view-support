688509bbf2795af2d56dfeb5763abab89b9d563c
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-09-09 18:28:32 +0300

    #14: Replace env variables by properties. Use docker gradle plugin.

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -2,17 +2,11 @@
 
 set -e
 
-. ./set-env-${DATABASE?}.sh
+docker="./gradlew ${DATABASE?}${MODE?}Compose"
 
-docker-compose -f docker-compose-${DATABASE?}-${MODE}.yml down -v
-docker-compose -f docker-compose-${DATABASE?}-${MODE}.yml up -d --build zookeeper kafka ${DATABASE?} mongodb
-
-./wait-for-${DATABASE?}.sh
-
-docker-compose -f docker-compose-${DATABASE?}-${MODE}.yml up -d --build cdcservice
-
-./wait-for-services.sh $DOCKER_HOST_IP "8099"
+${docker}Down
+${docker}Up
 
 ./gradlew build
 
-docker-compose -f docker-compose-${DATABASE?}-${MODE}.yml down -v
+${docker}Down
